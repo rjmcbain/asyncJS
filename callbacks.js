8e15edc9b1e8497e719fc2b7c601b8ad27d2541c
rjmcbain <rmcbain01@gmail.com>
2018-06-22 15:08:07 -0600

    adding callbacks

---
+++ callbacks.js
@@ -0,0 +1,16 @@
+const posts = [
+  { title: "Post One", body: "This is post one" },
+  { title: "Post Two", body: "This is post two" }
+];
+
+function getPosts() {
+  setTimeout(function() {
+    let output = "";
+    posts.forEach((post, index) => {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 3000);
+}
+
+getPosts();
